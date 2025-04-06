1912ff191cc5feac87efe7047d9a264bfc418d00
Tobiasz Bilinski <kysthalden@gmail.com>
2025-04-06 21:48:31 +0200

    Fix unused import

--- src/test/java/com/javaseleniumdemo/pages/BasePage.java
+++ src/test/java/com/javaseleniumdemo/pages/BasePage.java
@@ -2,7 +2,6 @@ package com.javaseleniumdemo.pages;
 
 import com.javaseleniumdemo.utils.WebDriverUtils;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class BasePage {
     protected WebDriver driver;
